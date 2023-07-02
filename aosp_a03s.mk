00a5f7037bf2fef2030db7e514636fae3f5bde76
ArcticAquila <franklinazriel6942@outlook.com>
2023-07-03 04:31:35 +0700

    Update aosp_a03s.mk

--- aosp_a03s.mk
+++ aosp_a03s.mk
@@ -3,13 +3,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+#$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from a03s device
-$(call inherit-product, device/samsung/a03s/device.mk)
+#$(call inherit-product, device/samsung/a03s/device.mk)
 
 PRODUCT_DEVICE := a03s
-PRODUCT_NAME := omni_a03s
+PRODUCT_NAME := aosp_a03s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A037F
 PRODUCT_MANUFACTURER := samsung
