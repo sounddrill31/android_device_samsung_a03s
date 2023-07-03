e60608ec43196ac6157d60313a3cd178d3fbea69
ArcticAquila <franklinazriel6942@outlook.com>
2023-07-03 23:44:09 +0000

    adding service

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
+ifneq ($(filter a03s, $(TARGET_DEVICE)),)
+
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),a03s)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
