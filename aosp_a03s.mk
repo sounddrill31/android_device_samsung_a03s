c6a0f4634481e6288b4cdec9a3b039b2a8f96e75
ArcticAquila <franklinazriel6942@outlook.com>
2023-07-04 06:29:55 +0700

    Update aosp_a03s.mk

--- aosp_a03s.mk
+++ aosp_a03s.mk
@@ -1,21 +1,38 @@
+
+DEVICE_PATH := device/samsung/a03s
+
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSP stuff.
 #$(call inherit-product, vendor/aosp/config/common.mk)
 
-# Inherit from a03s device
-#$(call inherit-product, device/samsung/a03s/device.mk)
+# Inherit some common TWRP stuff.                                             $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk) 
+
+# Inherit device configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)  
+ 
+               
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
+                                                                                                                                                                                                                             PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a03s
 PRODUCT_NAME := aosp_a03s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A037F
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-
+## Vendor Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="a03snnxx-user 12 SP1A.210812.016 A037FXXS3CWC7 release-keys"
 
