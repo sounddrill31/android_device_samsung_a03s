b1e768eb46e164e98ae01e38253e555382af2bf1
ArcticAquila <franklinazriel6942@outlook.com>
2023-07-04 07:01:21 +0700

    Update aosp_a03s.mk

--- aosp_a03s.mk
+++ aosp_a03s.mk
@@ -7,9 +7,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
-#$(call inherit-product, vendor/aosp/config/common.mk)
-
 # Inherit some common TWRP stuff.                                             $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Enable updating of APEXes
