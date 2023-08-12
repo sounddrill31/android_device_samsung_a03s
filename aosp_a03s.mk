bdf54b9844b212026b4508af2c1d18f906fb44e3
ArcticAquila <franklinazriel6942@outlook.com>
2023-08-12 20:48:10 +0700

    adding DEVICE_PATH variable

--- aosp_a03s.mk
+++ aosp_a03s.mk
@@ -4,13 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/samsung/a03s 
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk) 
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a03s/device.mk)  
+$(call inherit-product, $(DEVICE_PATH)/device.mk)  
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1600
