4d4d38912cc8f664835e1c8c5792d4ca2dcfca88
ArcticAquila <franklinazriel6942@outlook.com>
2023-09-27 22:28:11 +0700

    Revert "idk what i am doing again and again"
    This reverts commit 55cf3c113990c278a0430c78b7c935b3dd8a35c8.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,9 @@
-ifneq ($(filter a03s, $(TARGET_DEVICE)),)
-
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),a03s)
+
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 endif
--- device.mk
+++ device.mk
@@ -1,2 +1,21 @@
 LOCAL_PATH := device/samsung/a03s
 
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Product characteristics
+PRODUCT_CHARACTERISTICS := phone
+
+# Properties
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/samsung/a03s/a03s-vendor.mk)
