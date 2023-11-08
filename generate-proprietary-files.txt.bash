4cbaf8518dc0f504a6e88b43c76aa93d7eb7e721
ArcticAquila <franklinazriel6942@outlook.com>
2023-11-08 16:09:34 +0700

    adding proprietary-files.txt

---
+++ generate-proprietary-files.txt.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Find all files in the vendor directory that are not regular files
+find /vendor -type f ! -perm 0644 | while read -r file; do
+    # Extract the filename and path from the full path
+    filename=$(basename $file)
+    path=$(dirname $file)
+
+    # Calculate the relative path from the vendor directory
+    relative_path=${path#vendor/}
+
+    # Print the relative path and filename to the proprietary-files.txt file
+    echo "$relative_path/$filename" >> proprietary-files.txt
+done
