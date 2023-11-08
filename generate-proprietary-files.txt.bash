1119b1f1267ec2af6fa7ed7a5ad26c8d28ebf55f
ArcticAquila <franklinazriel6942@outlook.com>
2023-11-08 19:30:38 +0700

    Adding sha1 checksum

--- generate-proprietary-files.txt.bash
+++ generate-proprietary-files.txt.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Find all files in the vendor directory that are not regular files
+# Recursively find all files and directories in the vendor directory that are not regular files
 find /vendor -type f -o -type d ! -perm 0644 | while read -r file; do
     # Extract the filename and path from the full path
     filename=$(basename $file)
     path=$(dirname $file)
 
     # Calculate the relative path from the vendor directory
-    relative_path=${path#vendor/}
+    relative_path=${path#$vendor_dir/}
 
-    # Print the relative path and filename to the proprietary-files.txt file
-    echo "$relative_path/$filename" >> proprietary-files.txt
+    # Generate SHA-1 hash for the file
+    sha1sum=$(sha1sum "$file" | awk '{print $1}')
+
+    # Print the relative path, filename, and SHA-1 hash
+    echo "$relative_path/$filename|$sha1sum" >> proprietary-files.txt
 done
