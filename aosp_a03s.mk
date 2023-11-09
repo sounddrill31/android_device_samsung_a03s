be1e727585a3932660a95c2504fbe1f219d8d95e
ArcticAquila <franklinazriel6942@outlook.com>
2023-11-09 18:44:25 +0700

    Using key for sign if exist

--- aosp_a03s.mk
+++ aosp_a03s.mk
@@ -17,14 +17,17 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit some vendor files (if exists)
 $(call inherit-product-if-exists, vendor/samsung/a03s/a03s-samsung.mk)
 
-# Inherit keyfiles (Personal)
-($call inherit-if-exist)
+# Inherit keyfiles (Personal) (if exists)
+ifneq ($(wildcard vendor/extra/product.mk),)
+include vendor/extra/product.mk
+endif
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Product information
 PRODUCT_DEVICE := a03s
 PRODUCT_NAME := aosp_a03s
 PRODUCT_BRAND := samsung
@@ -32,10 +35,8 @@ PRODUCT_MODEL := SM-A037F
 PRODUCT_MANUFACTURER := samsung
 
 TARGET_GAPPS_ARCH := arm64
-
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="a03snnxx-user 12 SP1A.210812.016 A037FXXU4CWG6 release-keys"
-
 BUILD_FINGERPRINT := samsung/a03snnxx/a03s:12/SP1A.210812.016/A037FXXU4CWG6:user/release-keys
