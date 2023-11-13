5a7696c9dc17a207046659d3bc0ef2e2914e1c51
ArcticAquila <franklinazriel6942@outlook.com>
2023-11-13 18:17:56 +0700

    Add var for twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,3 +100,13 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_NTFS_3G := true
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 180
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/recovery.fstab
+
+# VNDK
+BOARD_VNDK_VERSION := current
