a37bb6fe8b3159fd2b84391aebf58d620023925f
ArcticAquila <franklinazriel6942@outlook.com>
2023-11-13 20:37:19 +0700

    Make it more simple

--- aosp_a03s.mk
+++ aosp_a03s.mk
@@ -1,27 +1,15 @@
 DEVICE_PATH := device/samsung/a03s
 
 # Inherit from those products. Most specific first.
-ifneq ($(realpath $(SRC_TARGET_DIR)/product/core_64_bit.mk),)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-endif
-
-ifneq ($(realpath $(SRC_TARGET_DIR)/product/full_base_telephony.mk),)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-endif
-
-ifneq ($(realpath $(SRC_TARGET_DIR)/product/updatable_apex.mk),)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-endif
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # pKVM
-ifneq ($(realpath packages/modules/Virtualization/apex/product_packages.mk),)
-$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
-endif
+$(call inherit-product-if-exists, packages/modules/Virtualization/apex/product_packages.mk)
 
 # Inherit some common AOSP stuff.
-ifneq ($(realpath vendor/aosp/config/common_full_phone.mk),)
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-endif
+$(call inherit-product-if-exists, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from a03s device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
