cc08d75416b1c275117f10cbf95c43b2c17f7f27
ArcticAquila <franklinazriel6942@outlook.com>
2023-11-14 18:40:22 +0700

    Thats exclusive for twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,7 @@ TW_INCLUDE_NTFS_3G := true
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 180
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/twrp.flags
 TW_SCREEN_BLANK_ON_BOOT := true
 
 # VNDK
